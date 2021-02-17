bdfa23b5faa4f39754260ac694b96388f31b5380
MP <35373980+MadhuraPotnis@users.noreply.github.com>
2021-02-17 10:15:52 +0530

    Update Oversampling_SMOTE.py

--- Signature Detection/Oversampling_SMOTE.py
+++ Signature Detection/Oversampling_SMOTE.py
@@ -10,15 +10,22 @@ from imblearn.over_sampling import SMOTE
 from collections import Counter
 from matplotlib import pyplot
 from sklearn.preprocessing import LabelEncoder
-print(imblearn.__version__)
 
-df = pd.read_csv (r'A:\BTech\BTech Project\Datasets\dataset_descriptor_copy.csv')
+df = pd.read_csv (r'A:\BTech\BTech Project\Datasets\Initial_Signature_Dataset.csv')
 print(df)
 data = df.values
 # split into input and output elements
 X, y = data[:, :-1], data[:, -1]
 # label encode the target variable
 y = LabelEncoder().fit_transform(y)
+# summarize distribution before SMOTE
+counter1 = Counter(y)
+for p,r in counter1.items():
+	per = r / len(y) * 100
+	print('Class=%d, n=%d (%.3f%%)' % (p, r, per))
+# plot the distribution
+pyplot.bar(counter1.keys(), counter1.values())
+pyplot.show()
 # transform the dataset
 oversample = SMOTE()
 X, y = oversample.fit_resample(X, y)
